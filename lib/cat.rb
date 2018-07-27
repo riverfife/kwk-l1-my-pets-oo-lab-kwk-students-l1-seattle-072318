2a1e4389137e07e70d1b1cb00b44c0c165296429
riverfife <riverfife@gmail.com>
2018-07-27 21:58:26 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,10 @@
 class Cat
   def initialize(name, mood)
-    @name = crookshanks
+    @name = name
     @mood = mood
   end
 end
 
+cat = Cat.new("crookshanks", "nervous")
 # attr_accessor :mood
 # attr_reader :name
